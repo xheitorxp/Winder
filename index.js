592e5a84face1a18f19a5e2617efa43c5432ee81
Williams <49321188+WillSilvah@users.noreply.github.com>
2023-02-16 21:20:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const aoijs = require("aoi.js");
 
-const aoifb = require("aoi.fb");
-
-<<<<<<< HEAD
 const fs = require('fs');
 
 const bot = new aoijs.AoiClient({
@@ -12,35 +9,15 @@ const bot = new aoijs.AoiClient({
     intents: ["Guilds", "GuildMembers", "GuildBans", "GuildEmojisAndStickers", "GuildIntegrations", "GuildWebhooks", "GuildInvites", "GuildVoiceStates", "GuildPresences", "GuildMessages", "GuildMessageReactions", "GuildMessageTyping", "DirectMessages", "DirectMessageReactions", "DirectMessageTyping", "MessageContent"],
      aoiLogs: false,
      events: ["onMessage", "onMessageDelete", "onMessageUpdate", "onMessageDeleteBulk", "onGuildUnavailable", "onChannelCreate", "onChannelUpdate", "onChannelDelete", "onJoin", "onLeave", "onMemberUpdate", "onMemberAvailable", "onBanAdd", "onBanRemove", "onReactionAdd", "onReactionRemove", "onReactionRemoveAll", "onReactionRemoveEmoji", "onInteractionCreate", "onApplicationCmdPermsUpdate", "onUserUpdate", "onVariableCreate", "onVariableDelete", "onVariableUpdate", "onFunctionError"],
-=======
-require('dotenv').config()
-console.log(process.env)
-
-const fs = require('fs');
-
-const bot = new aoijs.AoiClient({
-	token: process.env.TOKEN,
-	prefix: 'pw!',
-     intents: ["Guilds", "GuildMembers", "GuildBans", "GuildEmojisAndStickers", "GuildIntegrations", "GuildWebhooks", "GuildInvites", "GuildVoiceStates", "GuildPresences", "GuildMessages", "GuildMessageReactions", "GuildMessageTyping", "DirectMessages", "DirectMessageReactions", "DirectMessageTyping", "MessageContent"],
-     aoiLogs: false,
-     events: ["onMessage", "onMessageDelete", "onMessageUpdate", "onMessageDeleteBulk", "onGuildUnavailable", "onChannelCreate", "onChannelUpdate", "onChannelDelete", "onJoin", "onLeave", "onMemberUpdate", "onMemberAvailable", "onBanAdd", "onBanRemove", "onReactionAdd", "onReactionRemove", "onReactionRemoveAll", "onReactionRemoveEmoji", "onInteractionCreate", "onApplicationCmdPermsUpdate", "onUserUpdate", "onVariableCreate", "onVariableDelete", "onVariableUpdate", "onFunctionError"],
-     autoUpdates: false,
-     mobilePlatform: false,
-     respondToBots: false,
-    guildOnly: true,
->>>>>>> 4dafcab5eff965932ba187910ff0a306d3f20d23
      database: {
  type: "aoi.fb",
    tables: ["playboot-stable"],
  db: firebase
  },
-<<<<<<< HEAD
      autoUpdates: false,
      mobilePlatform: false,
      
      guildOnly: true,
-=======
->>>>>>> 4dafcab5eff965932ba187910ff0a306d3f20d23
         respondOnEdit: {
             commands: true,
         time: 600000
@@ -61,7 +38,6 @@ const files = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
 files.forEach( x => {
 require(`./events/${x}`)(bot)
 });
-<<<<<<< HEAD
 
 bot.variables({
 //configuração de canais padrão
@@ -74,5 +50,3 @@ bot.variables({
   "punishmentLog": "861367708716236800"
 
 })
-=======
->>>>>>> 4dafcab5eff965932ba187910ff0a306d3f20d23
