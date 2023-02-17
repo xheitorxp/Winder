011c70a83995a243b5f662b124e2eef64f9f2022
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-02-16 21:28:17 -0300

    Adicionar Capivaras a categoria Animais
    POR QUE NÃO???
    
    OBS: clica em pull, POR FAVOR

---
+++ comandos/Animais/capivara.js
@@ -0,0 +1,15 @@
+module.exports = {
+name: "capivara",
+aliases: ['capy'],
+desc: "Veja uma foto aleatória de uma capivara!",
+category: "Animal",
+usage: "capivara",
+code: `$botTyping
+<@$authorID>
+$attachment[$getObjectProperty[link];$username[$clientID]-capy.jpg]
+$createObject[$jsonRequest[https://api.animality.xyz/img/capybara]]
+$serverCooldown[30s;<@$authorID> | Espere **%time%** para executar este comando novamente!]
+$suppressErrors[Eu tentei executar o comando \`$commandName\` e eu juro que eu fiz o máximo possível.
+{error}]
+`
+} 
