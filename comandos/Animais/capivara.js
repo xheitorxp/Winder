74659598a8acedce7d159d7f5cee4905ea62e11a
Williams <49321188+WillSilvah@users.noreply.github.com>
2023-02-16 21:33:40 -0300

    Adicionado Capybara by Igor
    Categoria de Animais, usando Animality

--- comandos/Animais/capivara.js
+++ comandos/Animais/capivara.js
@@ -6,7 +6,7 @@ category: "Animal",
 usage: "capivara",
 code: `$botTyping
 <@$authorID>
-$attachment[$getObjectProperty[link];$username[$clientID]-capy.jpg]
+$attachment[$getObjectProperty[link];$username[$clientID]-capybara.jpg]
 $createObject[$jsonRequest[https://api.animality.xyz/img/capybara]]
 $serverCooldown[30s;<@$authorID> | Espere **%time%** para executar este comando novamente!]
 $suppressErrors[Eu tentei executar o comando \`$commandName\` e eu juro que eu fiz o máximo possível.
